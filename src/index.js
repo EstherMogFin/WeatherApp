b4ac722bae579c5cb4dbdc46797d6b749a134cda
Esther <117350560+EstherMF22@users.noreply.github.com>
2023-02-20 18:29:50 +0100

    implemented the unit conversion

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ function showWeather(response) {
   let wind = document.querySelector("#wind");
   let description = document.querySelector("#description");
   let emoji = document.querySelector("#emoji");
+  celsiusTemperature = response.data.main.temp;
   city.innerHTML = response.data.name;
   temperature.innerHTML = Math.round(response.data.main.temp);
   humidity.innerHTML = response.data.main.humidity;
@@ -56,6 +57,24 @@ function CurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
+function showFahrenheits(event) {
+  event.preventDefault;
+  temperatureElement = document.querySelector("#temperature");
+  fahrenheitsTemperature = Math.round(celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitsTemperature);
+}
+function showcelsius(event) {
+  event.preventDefault;
+  temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+let celsiusTemperature = null;
+
+let fahrenheits = document.querySelector("#fahrenheits");
+fahrenheits.addEventListener("click", showFahrenheits);
+
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", showcelsius);
 
 let form = document.querySelector("#country-form");
 form.addEventListener("submit", submit);
@@ -74,20 +93,3 @@ function changeTheme() {
 }
 let themeButton = document.querySelector("#theme-button");
 themeButton.addEventListener("click", changeTheme);
-
-function firstFreeLesson() {
-  let ready = prompt("Are you ready for this new adventure?🥊🖤");
-
-  let div = document.querySelector("div");
-  if (ready === "yes") {
-    div.innerHTML =
-      "Let's start training🥊🖤" +
-      "<br/>" +
-      "<video src='https://www.shutterstock.com/shutterstock/videos/1083548719/preview/stock-footage-woman-fighter-trains-his-punches-beats-a-punching-bag-training-day-in-the-boxing-gym-female.webm' controls></video>";
-  } else {
-    div.innerHTML =
-      "<img src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/063/249/original/TheOfficeDwightSchruteGIF.gif?1673962965'/>";
-  }
-}
-let firstFreeLessonButton = document.querySelector(".firstLesson");
-firstFreeLessonButton.addEventListener("click", firstFreeLesson);
