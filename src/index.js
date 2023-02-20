c2623945b9f01cd24a06789155e37fe4b05be341
Esther <117350560+EstherMF22@users.noreply.github.com>
2023-02-20 13:19:51 +0100

    newApp

---
+++ src/index.js
@@ -0,0 +1,59 @@
+let now = new Date();
+let date = document.querySelector("#exactDay");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+date.innerHTML = `${day}, ${hours}:${minutes}`;
+
+function showWeather(response) {
+  let city = document.querySelector("#city");
+  let temperature = document.querySelector("#temperature");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let description = document.querySelector("#description");
+  city.innerHTML = response.data.name;
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  description.innerHTML = response.data.weather[0].main;
+}
+
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function submit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#country-input").value;
+  search(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+search("Lecco");
+function CurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let form = document.querySelector("#country-form");
+form.addEventListener("submit", submit);
+
+let currentLocationButton = document.querySelector("#current");
+currentLocationButton.addEventListener("click", CurrentLocation);
